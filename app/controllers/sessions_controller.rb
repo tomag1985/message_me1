27492714a0936416cac349644208e52670be2c73
tomas agnese <tagnese1985@gmail.com>
2021-08-12 16:00:50 -0300

    create and destroy sessions actions now functioning

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,22 @@ class SessionsController < ApplicationController
 
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Successfully logged in :D'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Wrong credentials'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Successfully logged out :D'
+    redirect_to login_path
+  end
 end
